b99e426c6be86a7db699f6341fe9be80d7e2e56c
satriadhm <glorioussatria@gmail.com>
2025-02-15 11:27:43 +0700

    finish

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require("express");
+require("dotenv").config();
+const nodemailer = require("nodemailer");
+const bodyParser = require("body-parser");
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const cors = require("cors");
+app.use(cors());
+
+app.post("/send-email", (req, res) => {
+  const { name, email, message } = req.body;
+
+  const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465, // Port 465 untuk SSL atau 587 untuk TLS
+    secure: true, // true untuk port 465, false untuk port 587
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
+
+  const mailOptions = {
+    from: process.env.EMAIL_USER,
+    to: "orlenalycious@gmail.com",
+    subject: "New Contact Form Submission Orlenalycious",
+    text: `
+      Name: ${name}
+      Email: ${email}
+      Message: ${message}
+    `,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      res.status(500).send("Internal Server Error");
+    } else {
+      console.log("Email sent: " + info.response);
+      res.status(200).send("Email sent successfully");
+    }
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
